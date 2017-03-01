ae5d32ff799f2fbb93a2d92c69191fac428ad3ea
Les Aker <me@lesaker.org>
2017-02-28 19:38:06 -0500

    fix dynamic module loading

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,6 +7,18 @@ function reloadConfig(files)
         end
     end
 end
-local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+local confDir = os.getenv("HOME") .. "/.hammerspoon/"
+local confWatcher = hs.pathwatcher.new(confDir, reloadConfig):start()
+
+for module in hs.fs.dir(confDir) do
+    if module:sub(-4) == ".lua" then
+        local moduleName = module:sub(0, -5)
+        if moduleName ~= "init" then
+            require(module:sub(0, -5))
+        end
+    end
+end
+
 hs.alert.show("Config loaded")
 
